f9e21ee5e052c9e74b9528dc9a6ecd71ef6736da
Leonard Hovhannisyan <leonard.hovhannisyan@gmail.com>
2020-05-25 23:06:47 +0200

    Added 3 buttons. Implemented playing and stoping current song

--- Player.py
+++ Player.py
@@ -1,11 +1,65 @@
 import tkinter as tk
+import pygame as pg
+import os
 
-HEIGHT=650
-WIDTH=1100
+HEIGHT = 650
+WIDTH = 1100
+ALBUM_DIR = "/home/leon/DevelopmentDir/MP3Player/files/songs"
+IMG_DIR = "/home/leon/DevelopmentDir/MP3Player/files/images"
 
 root = tk.Tk()
+root.title("MP3 Player")
+pg.mixer.init()
+
+
+def play():
+    pg.init()
+    pg.mixer.init()
+    pg.mixer.music.load(song)
+    pg.mixer.music.play()
+
+
+def stop():
+    pg.mixer.music.stop()
+
+
+def pause():
+    return 0
+
+
+# def get_album():
+#     albumarray = []
+#     for i in range(3):
+#         albumarray.append(pg.mixer.music.load(os.path.join(ALBUM_DIR, '/music')))
+#
+#     return albumarray
+
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
+
+bg_img = tk.PhotoImage(file=IMG_DIR + "/background.png")
+bg_label = tk.Label(canvas, image=bg_img)
+bg_label.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+songFrame = tk.Frame(root, bg='#373737')
+songFrame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.2)
+playIcon = tk.PhotoImage(file=IMG_DIR + '/play.png') #PLAY
+playIcon = playIcon.subsample(5)
+playButton = tk.Button(songFrame, image=playIcon, command=play)
+playButton.place(relx=0, rely=0, relwidth=0.2, relheight=1)
+stopIcon = tk.PhotoImage(file=IMG_DIR + '/stop.png') #STOP
+stopIcon = stopIcon.subsample(5)
+stopButton = tk.Button(songFrame, image=stopIcon, command=stop)
+stopButton.place(relx=0.2, rely=0, relwidth=0.2, relheight=1)
+pauseIcon = tk.PhotoImage(file=IMG_DIR + '/pause.png') #PAUSE
+pauseIcon = pauseIcon.subsample(5)
+pauseButton = tk.Button(songFrame, image=pauseIcon, command=pause)
+pauseButton.place(relx=0.4, rely=0, relwidth=0.2, relheight=1)
+
+songListFrame = tk.Frame(root, bg='#373737')
+songListFrame.place(relx=0.1, rely=0.35, relwidth=0.8, relheight=0.5)
+
+song = ALBUM_DIR + '/music1.mp3'
 
 root.mainloop()
